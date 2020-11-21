2416600e90163517d92bd63ad26a6aef815de401
Gabriel <gabi_borges01@hotmail.com>
2020-11-21 18:38:07 -0300

    Criando o componente Comments. Testando funcionalidade (Hardcoding).

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
-import Header from './src/components/Header'
 import { View, Text } from 'react-native'
+
+// Components:
+import Header from './src/components/Header'
 import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
+    const comments = [{
+      nickname: 'Maria',
+      comment: 'Bela foto!'
+    }, {
+      nickname: 'João',
+      comment: 'Vish, faço melhor!! HAHAHA'
+    }]
+
     return(
       <View style={{ flex: 1}}>
         <Header />
-        <Post image={require('./assets/imgs/fence.jpg')} />
+        <Post image={require('./assets/imgs/fence.jpg')} comments={comments} />
       </View>
     )
   }
