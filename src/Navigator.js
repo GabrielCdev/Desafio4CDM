bf9adf12007d38747ece7b68a4c5a1b26e31376f
Gabriel <gabi_borges01@hotmail.com>
2020-11-22 12:59:40 -0300

    Implementando funcionalidade para Login e Logout

--- src/Navigator.js
+++ src/Navigator.js
@@ -24,7 +24,7 @@ const loginOrProfileRouter = createSwitchNavigator({
     Profile: Profile,
     Auth: authRouter
 }, {
-    initialRouteName: 'Profile'
+    initialRouteName: 'Auth'
 })
 
 const MenuRoutes = {
