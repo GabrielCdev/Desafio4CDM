09dae12b5f48c221396e3acb99d3fa7a68fce0ec
Gabriel <gabi_borges01@hotmail.com>
2020-11-22 11:43:12 -0300

    Integrando o storeConfig, reducers e action creators com a aplicação

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+import React from 'react'
+import { Provider } from 'react-redux'
 import registerRootComponent from 'expo/build/launch/registerRootComponent'
-
 import Navigator from './src/Navigator'
 
-registerRootComponent(Navigator)
+import storeConfig from './src/store/storeConfig'
+
+const store = storeConfig()
+
+const Redux = () => {
+    return(
+        <Provider store={store}>
+            <Navigator />
+        </Provider>
+    )
+}
+
+registerRootComponent(Redux)
