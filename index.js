dd93cd33574b8fb4503827b51d477aa0db36e6cd
Gabriel <gabi_borges01@hotmail.com>
2020-11-22 18:35:30 -0300

    Integrando o Firebase e testando

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import registerRootComponent from 'expo/build/launch/registerRootComponent'
 import React from 'react'
 import { Provider } from 'react-redux'
-import registerRootComponent from 'expo/build/launch/registerRootComponent'
+
 import Navigator from './src/Navigator'
 
 import storeConfig from './src/store/storeConfig'
 
-const store = storeConfig()
+import axios from 'axios'
+axios.defaults.baseURL = 'https://facsgram.firebaseio.com/'
 
+const store = storeConfig()
 const Redux = () => {
     return(
         <Provider store={store}>
